600f2bc28285fb1a50e9ede28bef46961aa841aa
Ainur Almukambetova <ainuralmukambetova@MacBook-Air-Ainur.local>
2023-01-25 12:25:27 +0100

    data exploration

--- test.py
+++
@@ -1,8 +0,0 @@
-import pandas as pd
-import numpy as np
-import matplotlib.pyplot as plt
-import seaborn as sns
-
-# creating the netflix dataframe 
-netflix_df = pd.read_csv('https://gist.githubusercontent.com/Ainuralmm/3bd1ebbaac091981f031ea47e7b18b61/raw/3132d3a22bdaa9edec8a639a3eae9987d25730bd/gistfile1.txt')
-print(netflix_df.head(5))
