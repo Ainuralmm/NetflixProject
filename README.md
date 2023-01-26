28d8877cd9c4dd0c9fd30276e842152e46b80ab0
Ainur Almukambetova <ainuralmukambetova@MacBook-Air-Ainur.local>
2023-01-26 14:45:51 +0100

    Added a description of the features in readme.md and completed the second part of DATA EXPLORATION

--- README.md
+++ README.md
@@ -1,1 +1,20 @@
 # NetflixProject
+FEATURES:
+
+SHOW-ID - Unique id of each show (not much of a use for us in this notebook)
+
+TYPE - The category of a show, can be either a Movie or a TV Show
+
+TITLE - Name of the show DIRECTOR - Name of the director(s) of the show
+
+CAST - Name of the show
+
+COUNTRY - Name of countries the show is available to watch on Netflix
+
+DATE ADDED - Date when the show was added on Netflix
+
+RATING - Show rating on netflix
+
+RELEASE YEAR - Release year of the show
+
+DURATION - Time duration of the show LISTED IN - Genre of the show
--- netflix.py
+++ netflix.py
@@ -1,25 +1,50 @@
+# FIRST PART. Import libries and creating the netflix dataframe 
+
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
 
-# creating the netflix dataframe 
 netflix_df = pd.read_csv('https://gist.githubusercontent.com/Ainuralmm/3bd1ebbaac091981f031ea47e7b18b61/raw/3132d3a22bdaa9edec8a639a3eae9987d25730bd/gistfile1.txt')
 print(netflix_df.head(5))
 
-# Data Exploration 
-# returning netflix dataframe values
+# SECOND PART. DATA EXPLORATION
+
+# showing netflix dataframe values
 print(netflix_df)
 
-# returning first 5 rows of netflix dataframe
+# showing first 5 rows of netflix dataframe
 print(netflix_df.head())
 
-# returning last 5 rows of netflix dataframe
+# showing last 5 rows of netflix dataframe
 print(netflix_df.tail())
 
-# returning the size of dataframe (rows, columns)
+# showing the size of dataframe (rows, columns)
 # netflix_df.shape
 print(f'Data has {netflix_df.shape[1]} columns & {netflix_df.shape[0]} rows.')
 
 # getting information like min,max and mean about numeric columns in netflix dataframe
 print(netflix_df.describe(include='all'))
+
+#same action like above but transposed
+print(netflix_df.describe(include='all').T)
+
+#To get the list of column headers in the netflix dataframe
+print(netflix_df.columns)
+
+#to clean the data set to remove a few unnecessary columns 
+netflix_df.drop(['show_id','date_added','rating'], axis=1, inplace=True)
+
+#let's return to see the result
+print(netflix_df)
+
+#so I removed unneseccery columns and now I want to rename some columns 
+netflix_df.rename(columns={'type':'genre', 'title':'movie_title'}, inplace=True)
+print(netflix_df.columns)
+
+#now I want to change headings' letters to capital letters
+netflix_df.columns = map(str.upper,netflix_df.columns)
+print(netflix_df.columns)
+
+# before moving to next cleaning part, I want to show how much the number of missing values in dataset
+print(netflix_df.isnull().sum())
