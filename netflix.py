1a5aa9e1f962b8a8709b925cfbbbddebdadebbe3
Ainur Almukambetova <ainuralmukambetova@MacBook-Air-Ainur.local>
2023-02-01 23:59:13 +0100

    added more exploration

--- netflix.py
+++ netflix.py
@@ -95,3 +95,52 @@ print(netflix_df['DURATION'].isnull().sum())
 print(netflix_df['DURATION'].notnull().sum())
 print(netflix_df.info())
 print(netflix_df.isnull().sum())
+
+
+#FOURTH PART. MORE EXPLORATION for Data Vizualiziotion
+#to show correlation I should change a type of 'duration' column to float
+netflix_df['DURATION']=np.float64(netflix_df['DURATION'])
+print(netflix_df.head())
+#correlation only between 2 columns, bcs other columns are strings
+print(netflix_df.corr())
+#number of movies and tv-shows in dataset
+print(netflix_df.GENRE.value_counts())
+#top 10 contries which create contets
+top_10_countries = netflix_df['COUNTRY'].value_counts()[0:10]
+print(top_10_countries)
+#another way to show top 10 contries which create content
+print(netflix_df.COUNTRY.value_counts().head(10))
+#the last 10 countires which create content
+print(netflix_df.COUNTRY.value_counts().tail(10))
+#the amount of content was created first 15 years
+print(netflix_df.RELEASE_YEAR.value_counts().head(15))
+#the amount of content was created last 15 years
+print(netflix_df.RELEASE_YEAR.value_counts().tail(15))
+#top 10 directors who create content
+top_10_directors = netflix_df['DIRECTOR'].value_counts()[0:10]
+print(top_10_directors)
+#the last 10 directors which create content
+print(netflix_df.DIRECTOR.value_counts().tail(10))
+#10 films of frequent duration in dataset
+print(netflix_df.DURATION.value_counts().head(10))
+#10 movies of rare duration
+print(netflix_df.DURATION.value_counts().tail(10))
+#the 10 most popular genres in dataset
+print(netflix_df.LISTED_IN.value_counts().head(10))
+#the most popular actors.but result is not correct bcs of comma in some rows
+print(netflix_df.CAST.value_counts().head(10))
+#so to get clear info about the 10 most popular i should to remove comma
+most_popular_10_actors = pd.concat([pd.Series(i.split(',')) for i in netflix_df.CAST]).value_counts().head(10)
+print(most_popular_10_actors)
+#dataframe where are showed only movies
+netflix_movie = netflix_df['GENRE']=='Movie'
+print(netflix_df[netflix_movie])
+#10 years with the highest total film releases
+highest_movie_releases=netflix_df[netflix_movie]['RELEASE_YEAR'].value_counts()[0:10]
+highest_movie_releases.name=None
+print(highest_movie_releases)
+#tv-shows dataframe 
+netflix_tvshows = netflix_df['GENRE']=='TV Show'
+print(netflix_df[netflix_tvshows])
+#5 years with the highest total tv-shows releases
+print(netflix_df[netflix_tvshows]['RELEASE_YEAR'].value_counts()[0:5])
